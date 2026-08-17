65b8d7eba2378a1938929b5b3b3920bcbcea2179
Kiyoshi Spreclerg <kiyoshi_pip@protonmail.com>
2026-08-17 14:35:56 -0300

    xispanel: bump version to 0.5.0
    Signed-off-by: Kiyoshi Spreclerg <kiyoshi_pip@protonmail.com>

--- xispanel/xispanel.c
+++ xispanel/xispanel.c
@@ -82,7 +82,7 @@
 #include <time.h>
 #include <unistd.h>
 
-#define XISPANEL_VERSION "0.4.1"
+#define XISPANEL_VERSION "0.5.0"
 #define MAX_PANELS 8
 #define LINE_MAX_LEN 2048
 #define IPC_MAX_LEN 4096
